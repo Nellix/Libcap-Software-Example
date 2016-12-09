c4e4d0324743b1381e0e442b5c7cf95ea754403e
Università <Uni@macbook-pro-di-aniello.local>
2016-12-09 16:16:19 +0100

    Initial Commit

---
+++ Risso_LIBCAP/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Risso_LIBCAP
+//
+//  Created by Università on 09/12/16.
+//  Copyright © 2016 Università. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
